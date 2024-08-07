7869e31e933a3878a728b4aca7cb2a00f16b8f06
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2024-08-07 22:59:53 +0500

    changing browserRouter to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import reportWebVitals from "./reportWebVitals";
@@ -11,9 +11,9 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter >
       <App />
-    </BrowserRouter>
+    </HashRouter >
   </Provider>
 );
 
